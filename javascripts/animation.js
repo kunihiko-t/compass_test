2a35d3666f4090d41e854e68f3f21f1379ccfe4d
Kunihiko Tanaka <inverse.of.void.matrix@gmail.com>
2013-05-12 07:03:04 +0900

    ちょっと変更

--- javascripts/animation.js
+++ javascripts/animation.js
@@ -9,4 +9,19 @@ $(function(){
 				innerDiv.attr("class","animation_box");
 		}
 	}
+
+	var func = function(){
+		var indexX = Math.floor( Math.random() * 10 );
+		var indexY = Math.floor( Math.random() * 10 );
+		var elem = jQuery(jQuery(".line")[indexY]).find("div")[indexX];
+		jQuery(elem).css({"background-color":"blue"});
+		console.log(indexX);
+		console.log(indexY);
+		setTimeout(function(){
+			func();
+		},1000);
+	}
+	setTimeout(function(){
+		func();
+	},1000);
 });
